c4217aa9d54e49d35732c5986557cc155a224ef4
cyber-nick <nikushachubunidze@gmail.com>
2024-10-05 14:51:04 +0400

    first commit

---
+++ day 1/lesson2.py
@@ -0,0 +1,17 @@
+name = "hello i am nika"
+
+surname = "my surname is chubunidze"
+
+age = "i am 15 years old"
+
+height = "and i am 178cm tall"
+
+knows_programming = "and im learning programming"
+
+# print(type(age))
+# print(type(name))
+# print(type(surname))
+# print(type(height))
+# print(type(knows_programming))
+
+print(name + " " + surname + " " + str(age) + " " + str(height) + " " + knows_programming)
