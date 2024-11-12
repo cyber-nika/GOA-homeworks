5471e957e47679e3cbbf70410db58ecb7482dcf9
cyber-nika <nikushachubunidze@gmail.com>
2024-11-12 16:34:23 +0400

    third commit

---
+++ day7/homework/classwork.py
@@ -0,0 +1,124 @@
+from pygame import *
+from random import randint
+
+font.init()
+font = font.Font(None, 36)
+
+img_win = "thumb.jpg"
+img_los = "game-over.png"
+img_back = "galaxy.jpg"
+
+img_bullet = "bullet.png"
+img_hero = "rocket.png"
+img_enemy = "ufo.png"
+
+score = 0
+goal = 10
+lost = 0
+max_lost = 3
+
+class GameSprite(sprite.Sprite):
+    def __init__(self, player_image, player_x, player_y, size_x, size_y, player_speed):
+        sprite.Sprite.__init__(self)
+        self.image = transform.scale(image.load(player_image), (size_x, size_y))
+        self.speed = player_speed
+        self.rect = self.image.get_rect()
+        self.rect.x = player_x
+        self.rect.y = player_y
+
+    def reset(self):
+        window.blit(self.image, (self.rect.x, self.rect.y))
+
+class Player(GameSprite):
+    def update(self):
+        keys = key.get_pressed()
+        if keys[K_LEFT] and self.rect.x > 5:
+            self.rect.x -= self.speed
+        if keys[K_RIGHT] and self.rect.x < win_width - 80:
+            self.rect.x += self.speed
+
+    def fire(self):
+        bullet = Bullet(img_bullet, self.rect.centerx, self.rect.top, 15, 20, -15)
+        bullets.add(bullet)
+
+class Enemy(GameSprite):
+    def update(self):
+        self.rect.y += self.speed
+        if self.rect.y > win_height:
+            self.rect.x = randint(80, win_width - 80)
+            self.rect.y = 0
+            global lost
+            lost = lost + 1
+
+class Bullet(GameSprite):
+    def update(self):
+        self.rect.y += self.speed
+        if self.rect.y < 0:
+            self.kill()
+
+win_width = 700
+win_height = 500
+display.set_caption("ლაბირინტი")
+window = display.set_mode((win_width, win_height))
+background = transform.scale(image.load(img_back), (win_width, win_height))
+
+ship = Player(img_hero, 5, win_height - 100, 80, 100, 10)
+
+monsters = sprite.Group()
+for i in range(1, 6):
+    monster = Enemy(img_enemy, randint(80, win_width - 80), -40, 80, 50, randint(1, 5))
+    monsters.add(monster)
+
+bullets = sprite.Group()
+
+finish = False
+run = True
+while run:
+    for e in event.get():
+        if e.type == QUIT:
+            run = False
+        elif e.type == KEYDOWN:
+            if e.key == K_SPACE:
+                ship.fire()
+
+    if not finish:
+        window.blit(background, (0, 0))
+
+        text = font.render("ქულა: " + str(score), 1, (255, 255, 255))
+        place = text.get_rect(center=(50, 20))
+        window.blit(text, place)
+
+        text_lose = font.render("დავალიანებული: " + str(lost), 1, (255, 255, 255))
+        place = text_lose.get_rect(center=(95, 50))
+        window.blit(text_lose, place)
+
+        ship.update()
+        monsters.update()
+        bullets.update()
+
+        ship.reset()
+        monsters.draw(window)
+        bullets.draw(window)
+
+        collides = sprite.groupcollide(monsters, bullets, True, True)
+        for c in collides:
+            score = score + 1
+            monster = Enemy(img_enemy, randint(80, win_width - 80), -40, 80, 50, randint(1, 5))
+            monsters.add(monster)
+
+        if sprite.spritecollide(ship, monsters, False) or lost >= max_lost:
+            finish = True
+            img = image.load(img_los)
+            d = img.get_width() // img.get_height()
+            window.fill((255, 255, 255))
+            window.blit(transform.scale(img, (win_height * d, win_height)), (90, 0))
+
+        if score >= goal:
+            finish = True
+            img = image.load(img_win)
+            window.fill((255, 255, 255))
+            window.blit(transform.scale(img, (win_width, win_height)), (0, 0))
+
+        display.update()
+
+    time.delay(50)
