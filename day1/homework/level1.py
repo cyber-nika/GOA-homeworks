95b7906624ec73b84a8784abf4cd2f784c4005dd
cyber-nika <nikushachubunidze@gmail.com>
2024-11-13 01:29:40 +0400

    Delete day1 directory

--- day1/homework/level1.py
+++
@@ -1,6 +0,0 @@
-name = "Nika"
-surname = "Chubunidze"
-age = 15
-height = 1.80
-knows_programming = False
-print("my name is" + " " + name + " " + "my surname is" + " " + surname + " " + "i am" + " " + str(age) + " " + "years old" + " " + "i am" + " " + str(height) + " " + "in heght")
